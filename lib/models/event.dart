daad70e9da9121b985946dedeee86fd5b0c9558e
Philip Eyram Djokoto <djoks@users.noreply.github.com>
2025-03-06 17:08:18 +0000

    wip

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,6 +1,7 @@
 import 'package:ticketmaster/models/price_range.dart';
 
 class Event {
+  final String id;
   final String name;
   final String thumbnail;
   final String banner;
@@ -11,6 +12,7 @@ class Event {
   final String? info;
 
   Event({
+    required this.id,
     required this.name,
     required this.thumbnail,
     required this.banner,
@@ -22,16 +24,14 @@ class Event {
   });
 
   factory Event.fromJson(Map<String, dynamic> json) {
-    // 1. Event Name
+    final id = json['id'] as String? ?? '';
     final name = json['name'] as String? ?? '';
 
-    // 2. Images: Choose thumbnail and banner from the images list.
     final List images = json['images'] as List? ?? [];
     String thumbnail = '';
     String banner = '';
     if (images.isNotEmpty) {
       thumbnail = images[0]['url'] as String? ?? '';
-      // Try to find an image with a "16_9" ratio for banner.
       final bannerImage = images.firstWhere(
         (img) => img['ratio'] == '16_9',
         orElse: () => null,
@@ -41,19 +41,16 @@ class Event {
           : thumbnail;
     }
 
-    // 3. Date and Time from dates -> start.
     final dates = json['dates']?['start'] ?? {};
     final date = dates['localDate'] as String? ?? '';
     final time = dates['localTime'] as String? ?? '';
 
-    // 4. Price: We take the first priceRange (if available).
     PriceRange? priceRange;
     if (json['priceRanges'] is List &&
         (json['priceRanges'] as List).isNotEmpty) {
       priceRange = PriceRange.fromJson((json['priceRanges'] as List).first);
     }
 
-    // 5. Location: From _embedded -> venues -> first venue's name.
     String location = '';
     if (json['_embedded'] != null &&
         json['_embedded']['venues'] is List &&
@@ -61,10 +58,10 @@ class Event {
       location = (json['_embedded']['venues'][0]['name'] as String?) ?? '';
     }
 
-    // 6. Event info (if available).
     final info = json['info'] as String?;
 
     return Event(
+      id: id,
       name: name,
       thumbnail: thumbnail,
       banner: banner,
